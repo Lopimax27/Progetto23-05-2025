26028186d5abb10be5d5123410425ce026f09acf
Besky037 <Leo.Betti05@gmail.com>
2025-05-23 17:36:45 +0200

    Update Leo
    Operatore e Meccanico

--- Leo/ProgettoLeo/Program.cs
+++ Leo/ProgettoLeo/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class OperatoreCentrale : Dipendente
+{
+    public string Turno
+    {
+        get { return turno; }
+        set
+        {
+            if (value.ToLower() == "giorno" || value.ToLower() == "notte")
+            {
+                turno = value.ToLower();
+            }
+        }
+    }
+    public OperatoreCentrale(string nome, int eta, string turno) : base(nome, eta)
+    {
+        Turno = turno;
+    }
+    public virtual override EseguiCompito()
+    {
+        Console.Writeline($"Gestisce le comunicazioni in turno {Turno}")
+    }
+}
+public class Meccanico : Dipendente 
+{
+    public string Specializzazione { get; set; }
+    public Meccanico(string nome, int eta, string specializzazione) : base(nome, eta)
+    {
+        Specializzazione = specializzazione;
+    }
+    public virtual override EseguiCompito()
+    {
+        Console.Writeline($"Ripara mezzi specializzati in {Specializzazione}")
+    }
+}
